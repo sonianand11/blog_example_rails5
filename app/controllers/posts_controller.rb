ca40bc61b1e446b36b21063a9f9e299aca4ca593
Anand Soni <sonianand11@gmail.com>
2018-06-20 13:08:55 +0530

    allow user to show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
